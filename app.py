8494bcc4579419eec5ef21df6937d8fb7bd9679b
MrMorley-PC <morley.william.wm@gmail.com>
2025-11-12 23:05:49 +0200

    folium map rendering resolved

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def main():
     with col1:
         selected_species = st.selectbox("Filter by Species", all_species)
         m = create_map(df, selected_species)
-        st_folium(m, width=1200, height=700)
+        st_folium(m, width=1200, height=700, returned_objects=[]) # resolves the map rendering bug
     
     with col2:
         st.subheader("📊 Summary")
